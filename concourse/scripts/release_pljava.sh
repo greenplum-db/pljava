b9fc9afdbb0c1dd8f4732c7f40edcefe9c27a799
Hao Wu <hawu@pivotal.io>
2018-10-23 16:36:40 +0800

    Use tag to name the package, instead of hardcoded

--- concourse/scripts/release_pljava.sh
+++ concourse/scripts/release_pljava.sh
@@ -5,16 +5,15 @@ set -exo pipefail
 CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TOP_DIR=${CWDIR}/../../../
 
-
 function release() {
     mkdir -p pljava_gppkg
-
+    PLJAVA_VERSION=$(git describe --abbrev=0)
     case "$OSVER" in
         suse11)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.3.1-gp4-sles11-x86_64.gppkg
+        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-${PLJAVA_VERSION}-gp4-sles11-x86_64.gppkg
         ;;
         centos5)
-        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-1.3.1-gp4-rhel5-x86_64.gppkg
+        cp pljava_bin/pljava-*.gppkg pljava_gppkg/pljava-${PLJAVA_VERSION}-gp4-rhel5-x86_64.gppkg
         ;;
         *) echo "Unknown OS: $OSVER"; exit 1 ;;
     esac
