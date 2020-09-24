f54fbefcce5f88cf32b7e153235ddb3ab3da353c
Haozhou Wang <hawang@pivotal.io>
2020-09-24 11:22:03 +0800

    Remove -Xmx option to match upsteam and update minimum JDK requirement to 8 (#45)

--- pljava-so/src/main/c/Backend.c
+++ pljava-so/src/main/c/Backend.c
@@ -281,8 +281,6 @@ static void initsequencer(enum initstage is, bool tolerant)
 		if ( ! seenVisualVMName )
 			JVMOptList_addVisualVMName(&optList);
 		JVMOptList_add(&optList, "vfprintf", (void*)my_vfprintf, true);
-		/* add -xmx to aviod heap error */
-		JVMOptList_add(&optList, "-Xmx512m", 0, true);
 		JVMOptList_add(&optList, "-Xss2m", 0, true);
 #ifndef GCJ
 		JVMOptList_add(&optList, "-Xrs", 0, true);
