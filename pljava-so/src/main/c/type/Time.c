cec40bd44537c1736f3351e952c4752a8eb7c8dc
Chen Mulong <chenmulong@gmail.com>
2023-01-11 12:03:02 +0800

    Handle the retirement of nabstime. (#54)
    Upstream cda6a8d makes nabstime.h go away. GetCurrentAbsoluteTime()
    was being used here, but didn't do anything time(NULL) doesn't do.
    
    (cherry picked from commit a29e0024e4d50ac073039b0430785593f45f87f7)
    
    Co-authored-by: Chapman Flack <chap@anastigmatix.net>

--- pljava-so/src/main/c/type/Time.c
+++ pljava-so/src/main/c/type/Time.c
@@ -1,13 +1,20 @@
 /*
- * Copyright (c) 2004, 2005, 2006 TADA AB - Taby Sweden
- * Distributed under the terms shown in the file COPYRIGHT
- * found in the root folder of this project or at
- * http://eng.tada.se/osprojects/COPYRIGHT.html
+ * Copyright (c) 2004-2019 Tada AB and other contributors, as listed below.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the The BSD 3-Clause License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Contributors:
+ *   Tada AB
+ *   Chapman Flack
  *
  * @author Thomas Hallgren
  */
+#include <sys/time.h>
+
 #include <postgres.h>
-#include <utils/nabstime.h>
 #include <utils/date.h>
 #include <utils/datetime.h>
 
@@ -29,7 +36,7 @@ static jmethodID s_Time_getTime;
 
 static jlong msecsAtMidnight(void)
 {
-	AbsoluteTime now = GetCurrentAbsoluteTime() / 86400;
+	pg_time_t now = (pg_time_t)time(NULL) / 86400;
 	return INT64CONST(1000) * (jlong)(now * 86400);
 }
 
