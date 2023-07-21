09c3b08f04968abe85ae5108bf0f277564d8cebc
liuxueyang <liuxueyang457@gmail.com>
2023-07-21 14:36:48 +0800

    Fix shell script log bug

--- concourse/scripts/test_pljava.sh
+++ concourse/scripts/test_pljava.sh
@@ -23,6 +23,7 @@ function _main() {
         echo "JAVA_VERSION must be specified"
         return 1
     fi
+    JAVA_VERSION="$1"
     echo "JAVA_VERSION=$JAVA_VERSION"
 
     local tmp_dir
@@ -38,7 +39,7 @@ function _main() {
 
     pushd /home/gpadmin/pljava_src
     # compile examples using current JDK version and run tests
-    JAVA_VERSION=$1 make examples
+    JAVA_VERSION="$JAVA_VERSION" make examples
     time make targetcheck || cat gpdb/tests/regression.diffs
     # restore examples to original state or gppkg will failed to checksum
     make restore_examples
