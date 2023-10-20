c3a89c7bd313ef88532e25dd00ae63c3ee79e469
Chen Mulong <chenmulong@gmail.com>
2023-10-20 13:05:24 +0800

    order is important

--- concourse/pre_test.sh
+++ concourse/pre_test.sh
@@ -5,8 +5,8 @@ set -ex
 source "$CI_REPO_DIR/common/entry_common.sh"
 source "$(dirname "$0")/common.sh"
 
-# Create GPDB cluster
-start_gpdb
-
 install_java "$1"
 setup_maven
+
+# Create GPDB cluster
+start_gpdb
